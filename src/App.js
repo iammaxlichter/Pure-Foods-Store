4930ffb26429c3b59dbba79c3861a62a21c4b4ac
Max Lichter <iammaxlichter@gmail.com>
2024-07-07 19:14:14 -0500

    started pure foods store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Assuming you have some CSS for styling your sections
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="text-center">Hello, Bootstrap!</h1>
+      <button className="btn btn-primary">Click Me</button>
     </div>
   );
 }
